881550d3a54ef1e2fc6f4e09e1fa560d4512aa4f
Hammad-181 <hammadfasih8@gmail.com>
2022-03-14 23:46:47 +0530

    inital message

---
+++ script.js
@@ -0,0 +1,444 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+/////////////////////////////////////////////////
+// Data
+
+// DIFFERENT DATA! Contains movement dates, currency and locale
+
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+
+  movementsDates: [
+    '2019-11-01T13:15:33.035Z',
+    '2019-11-30T09:48:16.867Z',
+    '2019-12-25T06:04:23.907Z',
+    '2020-01-25T14:18:46.235Z',
+    '2020-02-05T16:33:06.386Z',
+    '2020-04-10T14:43:26.374Z',
+    '2020-06-25T18:49:59.371Z',
+    '2020-07-26T12:01:20.894Z',
+  ],
+  currency: 'USD',
+  locale: 'en-US',
+};
+const account3 = {
+  owner: 'Hammad Fasih',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 3333,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
+
+const account4 = {
+  owner: 'Zaid Hussain',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 4444,
+
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
+
+const accounts = [account1, account2, account3, account4];
+
+/////////////////////////////////////////////////
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+// Functions
+
+const displayMovements = function (movements, sort = false) {
+  containerMovements.innerHTML = '';
+
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+const calcDisplayBalance = function (acc) {
+  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
+};
+
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
+    .filter(mov => mov > 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      // console.log(arr);
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
+};
+
+const createUsernames = function (accs) {
+  accs.forEach(function (acc) {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+createUsernames(accounts);
+
+const updateUI = function (acc) {
+  // Display movements
+  displayMovements(acc.movements);
+
+  // Display balance
+  calcDisplayBalance(acc);
+
+  // Display summary
+  calcDisplaySummary(acc);
+};
+
+///////////////////////////////////////
+// Event handlers
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === +inputLoginPin.value) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
+btnTransfer.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = +inputTransferAmount.value;
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+  inputTransferAmount.value = inputTransferTo.value = '';
+
+  if (
+    amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?.username !== currentAccount.username
+  ) {
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  const amount = Math.floor(inputLoanAmount.value);
+
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    +inputClosePin.value === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+    // .indexOf(23)
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+});
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+/*
+//Numbers
+console.log(20 === 20.0); 
+
+//ways to convert strings into numbers
+//conversion
+
+console.log(Number("21"));
+console.log(+ "21");  // just add + will do coersion and make the string 
+
+//parsing
+
+console.log(Number.parseInt("50c" , 10));  // parseint extract the numbers from strings and second parameter is regex which is base which we are using. 
+
+console.log(Number.parseFloat("2.5rem")); //parsefloat extract whole number including decimal part but parseInt only extract num
+
+console.log(Number.isNaN(20));  //checks wheather its a number or not
+console.log(Number.isNaN("20")); 
+console.log(Number.isNaN(+ "21x")); 
+
+//This method is correct to check if it is number or not instead of isNan
+
+console.log(Number.isFinite(20)); 
+console.log(Number.isFinite("20"));
+console.log(Number.isFinite(+ "20px"));
+console.log(Number.isFinite (20/0));  
+
+
+//Math functions
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1/2)); //for aquare root
+console.log(8 ** (1/3)); //for cubic root similarly for quadratic we can put 4 in denominator
+
+//Max
+
+console.log(Math.max(1,2,6,29,3,4));  //returns max val
+
+//min
+
+console.log(Math.min(1, 9,3,2));  //returns min vslue
+
+console.log(Math.PI); 
+
+//Math random
+
+console.log(Math.trunc(Math.random() * 6) + 1); 
+
+const randomInt = (max, min) => Math.floor(Math.random () * (max - min) + 1) + min;
+
+console.log(randomInt(2, 10)); 
+
+
+//Rounding Integers
+
+console.log(Math.trunc(23.7)); //Math.trunc remove the integer 
+
+console.log(Math.round(23.7)); //it returns the round figure integer
+
+console.log(Math.ceil(23.7));  //Math.ceil return max rounded value
+console.log(Math.ceil(23.1));
+
+console.log(Math.floor(23.9));  //math.floor returns min rounded value
+
+//Rounding decimals
+
+console.log((2.7).toFixed(0)); // to fixed return strings on float and (0) it takes the upto decimal we want to return
+
+console.log((2.7234).toFixed(2));
+
+console.log(+(2.76).toFixed(2)); 
+
+
+
+//remainder opeartor
+
+console.log(5 % 2);
+console.log(8 % 3);
+
+const isEven = num => num % 2 === 0;
+console.log(isEven(6));
+console.log(isEven(3));  
+
+labelBalance.addEventListener("click", function(){
+  
+  [...document.querySelectorAll(".movements__row")].forEach(function(row, i){
+    
+    if(i % 2 === 0){
+      row.style.backgroundColor = "orangered" ;
+     
+    }
+    if(i % 3 === 0){
+      row.style.backgroundColor = "blue" ;
+      
+    }
+  })  
+
+ 
+console.log(2 ** 53 - 1);  //max safe integer
+
+//transforming a regular number into BigInt number.
+
+console.log(34878734673571297723024n);  //add n at last to convert any number into BigInt
+
+//console.log(20n * 2);  // cant multiply big int with numbers
+
+//operations
+console.log(10n < 11);  // true  
+console.log(10n === 10);  // false
+
+
+//operations
+
+const hugeNum = 3446572370927095237925721n;
+const nums = 3487343489;
+
+console.log(hugeNum * BigInt(nums));
+
+//concatination
+
+console.log(hugeNum + "is a big number");
+
+//divisions
+
+console.log(10n/3n); 
+
+ */
+
+const now = new Date();
+console.log(now);
+
+console.log(new Date('15 December, 1999'));
+console.log(account1.movementsDates[0]);
+console.log(new Date(1999, 11, 15));
+
+//converting a number into milisecond
+
+console.log(2 * 24 * 60 * 60 * 1000);
+
+//working with dates
+
+const future = new Date(2025, 8, 7, 10, 12);
+console.log(future);
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(1757220120000));
